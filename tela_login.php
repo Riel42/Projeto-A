dc1d3600d523818319d19d939f836c6271541695
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-03-14 15:06:15 -0300

    first commit

---
+++ tela_login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Tela de Login - Bebel</title>
+</head>
+<body>
+    <section id="cxprincipal">
+        <h1>Tela de Login</h1>
+        <figure id="cxcadeado">
+            <img src="img/cadeado.png" alt="Mii characters" class="fig1" width="250px" height="280px">
+        </figure>
+        
+        <header id="cxlogin">
+            <form action="#" method="post">
+                <label for="email">E-mail: </label><br>
+                <input type="email" name="email" id="email"><br>
+                <label for="senha">Senha: </label><br>
+                <input type="password" name="passwd" id="passwd"><br><br>
+                <input type="submit" value="Acessar!"><br>
+                <label for="mensage"><a href="#">Cadastre-se aqui :3</a></label>
+            </form>
+        </header>
+    </section>
+</body>
+</html>
